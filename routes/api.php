9dbb09dbcae20480a8c9dd8c503f09359d894924
Marco Antonio Sanchez M <masmctt@gmail.com>
2018-04-22 17:38:04 -0500

    S7 Creando los Componentes iniciales de Laravel para la API RESTfil

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,34 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+/**
+* Buyers
+*/
+Route::resource('buyers','Buyer\BuyerController',['only' => ['index','show']]);
+/**
+* Categories
+*/
+
+Route::resource('categories','Category\CategoryController',['except' => ['create','edit']]);
+/**
+* Products
+*/
+Route::resource('products','Product\ProductController',['only' => ['index','show']]);
+/**
+* Transactions
+*/
+Route::resource('transactions','Transaction\TransactionController',['only' => ['index','show']]);
+/**
+* Sellers
+*/
+Route::resource('sellers','Seller\SellerController',['only' => ['index','show']]);
+/**
+* Users
+*/
+Route::resource('users','user\UserController',['except' => ['create','edit']]);
