c43424a358251e9d228804b1a53fab6c54d4e59c
masmctt <masmctt@gmail.com>
2018-04-23 19:25:33 -0500

    Generando la Estructura de la Base de Datos usando Migraciones de Laravel

--- database/migrations/2018_04_22_000344_create_products_table.php
+++ database/migrations/2018_04_22_000344_create_products_table.php
@@ -15,7 +15,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description',1000);
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
+            $table->string('image');
+            $tabke->intger('seller_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('seller_id')->reference('id')->on('users');
         });
     }
 
